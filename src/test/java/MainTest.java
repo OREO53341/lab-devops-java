651a392ef13453890c186b50cdef29399e86d691
ADEL <zieradel@gmail.com>
2026-07-11 09:35:55 +0200

    plugin JaCoCo pour la couverture

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -3,11 +3,7 @@ import org.junit.jupiter.api.Test;
 public class MainTest {
     @Test
     public void testMain() {
-        // On crée un tableau de chaînes vide pour les arguments
         String[] args = {};
-        
-        // On appelle explicitement la méthode main de la classe Main
-        // Cela va forcer JaCoCo à traverser le code de Main.java !
         Main.main(args);
     }
 }
